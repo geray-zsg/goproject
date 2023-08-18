cc4511de980aa4932a1224bf9e7795fa4af93390
geray <1690014753@qq.com>
2023-08-18 13:14:56 +0800

    ks对接租户门户url编解码

---
+++ src/gocode/encodeURL/main.go
@@ -0,0 +1,50 @@
+/**
+ * @Author: Geray
+ * @Date: 2023/8/18 12:52:26
+ * @LastEditors: Geray
+ * @LastEditTime: 2023/8/18 12:52:26
+ * Description: 实现URL编解码
+   容器平台提供给租户门户侧url地址为：租户门户地址+容器平台侧kubesphere-config中配置的cas地址的编码
+   		租户门户地址：https://it.cmft/gateway/cas?service=
+		ks配置文件配置地址：redirectURL: http://ks.cloud.cmft:30880/oauth/redirect/cas（编码后进行拼接：http%3A%2F%2Fks.cloud.cmft%3A30880%2Foauth%2Fredirect%2Fcas）
+
+		结果：https://it.cmft/gateway/cas?service=http%3A%2F%2Fks.cloud.cmft%3A30880%2Foauth%2Fredirect%2Fcas
+
+ * Copyright: Copyright (©)}) 2023 Geray. All rights reserved.
+*/
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+// 编码
+func setEncodedURL(urlStr string) {
+	encodeURL := url.QueryEscape(urlStr)
+	fmt.Println("编码结果", encodeURL)
+}
+
+// 解码
+func getDecodeURL(urlStr string) {
+	decodeURL, err := url.QueryUnescape(urlStr)
+	if err != nil {
+		fmt.Println("解码失败：", err)
+		return
+	}
+	fmt.Println("解码结果：", decodeURL)
+}
+
+func main() {
+
+	// 编码
+	// encodeURLStr := "http://ks.cloud.cmft:30880/oauth/redirect/cas"
+	encodeURLStr := "http://10.127.128.10:30880/oauth/redirect/cas"
+	setEncodedURL(encodeURLStr)
+
+	// URL
+	decodeURLStr := "https://it.cmft/gateway/cas?service=http%3A%2F%2F10.127.128.10%3A30880%2Foauth%2Fredirect%2Fcas"
+	// 解码
+	getDecodeURL(decodeURLStr)
+
+}
